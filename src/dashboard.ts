28a9e535edbe0aaf8768526e2346f0bbd1e00ea3
Costa <costat@pt.glory-global.com>
2018-09-11 10:17:00 +0100

    initial structure

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -1,13 +1,27 @@
+import { Utils } from './utils'
 import 'core-js/fn/promise'
 
 export class Dashboard {
   public static GetCurrent(): Promise<IDashboard> {
     return new Promise((resolve, reject) => {
       try {
+        const win = <any>window
         const viewState = <any>document.getElementById('idViewStateDiv')
         const path: string = viewState.getAttribute('statepath')
-        const args = (<any>window).obips.views.ViewController.getController(path).viewEnv.argMap
-        resolve({ Name: args.DashboardCaption, Path: args.PortalPath, Controller: path })
+        const controller = <IController>win.obips.views.ViewController.getController(path)
+
+        let result: IDashboard = {
+          Name: controller.getViewEnvParam('DashboardCaption'),
+          Path: controller.getViewEnvParam('PortalPath'),
+          Page: controller.getViewEnvParam('Page'),
+          Controller: path,
+          FriendlyName: ''
+        }
+
+        result.FriendlyName = Utils.NormalizeString(`${result.Name} ${result.Page}`)
+          .toLowerCase()
+          .replace(/[^\w]/g, '_')
+        resolve(result)
       } catch (e) {
         reject(e)
       }
@@ -19,4 +33,11 @@ export interface IDashboard {
   Name: string
   Path: string
   Controller: string
+  Page: string
+  FriendlyName: string
+}
+
+export interface IController {
+  getViewEnvParam(name: string): string
+  viewEnv: any
 }
--- src/prompts.ts
+++ src/prompts.ts
@@ -1,7 +1,6 @@
 import { Utils } from './utils'
 import { Dashboard, IDashboard } from './dashboard'
 import Cookies from 'js-cookie'
-
 import 'core-js/fn/promise'
 
 export class Prompts {
@@ -9,11 +8,12 @@ export class Prompts {
   //https://community.oracle.com/thread/3730170
   //http://obi2ru.blogspot.com/2013/02/biee11g-set-presentation-variable-by-javascript.html
 
-  private static GetPromptManager(): Promise<any> {
+  private static GetPromptManager(): Promise<IPromptManager> {
     return new Promise((resolve, reject) => {
-      const pm = (<any>window).PromptManager
-      if (typeof pm === 'object') {
-        resolve(pm.getPromptManager())
+      const win = <any>window
+      const pm = <IPromptManager>win.PromptManager
+      if (typeof pm !== 'undefined') {
+        resolve(pm)
       } else {
         console.error('Not running under OTBI')
         reject()
@@ -21,9 +21,9 @@ export class Prompts {
     })
   }
 
-  public static GetByCaption(caption: string): Promise<any> {
+  public static GetByCaption(caption: string): Promise<IPrompt> {
     return this.GetPromptManager().then(pm => {
-      const collections = pm.getAllPromptCollectionJSON()
+      const collections = pm.getPromptManager().getAllPromptCollectionJSON()
 
       for (let i = 0; i < collections.length; i++) {
         const c = collections[i]
@@ -32,18 +32,21 @@ export class Prompts {
           for (let k = 0; k < pstep.prompts.length; k++) {
             const p = pstep.prompts[k]
             if (p.caption === caption) {
-              return p
+              p.viewStatePath = c.viewStatePath
+              return Promise.resolve(p)
             }
           }
         }
       }
+
+      return Promise.reject(null)
     })
   }
 
-  public static GetByType(promptType: string, controlType: string): Promise<any[]> {
+  public static GetByType(promptType: string, controlType: string): Promise<IPrompt[]> {
     return this.GetPromptManager().then(pm => {
-      const collections = pm.getAllPromptCollectionJSON()
-      let result: any[] = []
+      const collections = pm.getPromptManager().getAllPromptCollectionJSON()
+      let result: IPrompt[] = []
 
       for (let i = 0; i < collections.length; i++) {
         const c = collections[i]
@@ -52,36 +55,45 @@ export class Prompts {
           for (let k = 0; k < pstep.prompts.length; k++) {
             const p = pstep.prompts[k]
             if (p.promptType === promptType && p.uiControlType === controlType) {
+              p.viewStatePath = c.viewStatePath
               result.push(p)
             }
           }
         }
       }
 
-      return result
+      return Promise.resolve(result)
     })
   }
 
   public static SetPrompt(caption: string, value: any, update: boolean): void {
-    let promises = []
+    let promises: Promise<any>[] = []
     promises.push(this.GetPromptManager())
     promises.push(this.GetByCaption(caption))
 
-    Promise.all(promises).then(result => {
-      let pm = result[0]
-      let pInfo = result[1]
+    let pm: IPromptManager
+    let pInfo: IPrompt
 
-      let expression =
-        "<sawx:expr xmlns:sawx='com.siebel.analytics.web/expression/v1.1' xsi:type='sawx:list' op='_OP_' emptyAsAllChoices='true' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><sawx:expr xsi:type='sawx:sqlExpression'>_COLFORMULA_</sawx:expr><sawx:expr xsi:type='xsd:string'>_VAL_</sawx:expr></sawx:expr>"
-      expression = expression
-        .replace('_COLFORMULA_', pInfo.dataType.displayColumnFormula)
-        .replace('_VAL_', value)
-        .replace('_OP_', pInfo.operator)
-      let xml = Utils.GetXML(expression)
+    Promise.all(promises)
+      .then(result => {
+        pm = <IPromptManager>result[0]
+        pInfo = <IPrompt>result[1]
 
-      var action = update ? pm.FINISH_ACTION : pm.NEXT_ACTION
-      pm.submitPrompt(pInfo.viewStatePath, update, action, xml)
-    })
+        let expression =
+          "<sawx:expr xmlns:sawx='com.siebel.analytics.web/expression/v1.1' xsi:type='sawx:list' op='_OP_' emptyAsAllChoices='true' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><sawx:expr xsi:type='sawx:sqlExpression'>_COLFORMULA_</sawx:expr><sawx:expr xsi:type='xsd:string'>_VAL_</sawx:expr></sawx:expr>"
+        expression = expression
+          .replace('_COLFORMULA_', pInfo.dataType.displayColumnFormula)
+          .replace('_VAL_', value)
+          .replace('_OP_', pInfo.operator)
+
+        return Utils.GetXML(expression)
+      })
+      .then(v => {
+        if (pInfo.viewStatePath) {
+          const action = update ? pm.FINISH_ACTION : pm.NEXT_ACTION
+          pm.submitPrompt(pInfo.viewStatePath, update, action, v)
+        }
+      })
   }
 
   public static SaveAll(): void {
@@ -91,7 +103,7 @@ export class Prompts {
 
     Promise.all(promises).then(result => {
       let dashboard = <IDashboard>result[0]
-      let dropdowns = result[1]
+      let dropdowns = <IPrompt[]>result[1]
       let saveFilters: any = {}
 
       //Handle dropdowns
@@ -106,7 +118,11 @@ export class Prompts {
           for (let j = 0; j < current.currentValues.values.length; j++) {
             let value = current.currentValues.values[j]
             if (value.caption && value.codeValue && value.codeValue !== '*)nqgtac(*') {
-              currentFilter.push({ caption: value.caption, code: value.codeValue })
+              currentFilter.push({
+                caption: value.caption,
+                code: value.codeValue,
+                controlType: 'dropDown'
+              })
             }
           }
           if (currentFilter.length > 0) {
@@ -115,15 +131,72 @@ export class Prompts {
         }
       }
 
-      Cookies.set(dashboard.Name, saveFilters, { expires: 365 })
+      Cookies.set(dashboard.FriendlyName, saveFilters, { expires: 365 })
     })
   }
 
   public static LoadAll(): void {
     Dashboard.GetCurrent().then(dashboard => {
-      let savedFilters = Cookies.get(dashboard.Name)
+      let savedFilters = Cookies.getJSON(dashboard.FriendlyName)
+      let keys = Object.keys(savedFilters)
 
-      //Handle dropdowns
+      for (let i = 0; i < keys.length; i++) {
+        const refresh = i === keys.length - 1
+        const key = keys[i]
+        const values = savedFilters[key]
+        let value = ''
+
+        for (let j = 0; j < values.length; j++) {
+          if (j > 0) value += ';'
+          value += values[j].caption
+        }
+
+        this.SetPrompt(key, value, refresh)
+      }
     })
   }
+}
+
+export interface IPromptManager {
+  FINISH_ACTION: string
+  NEXT_ACTION: string
+  submitPrompt(path: string, update: boolean, action: string, xml: any): void
+  getPromptManager(): IPromptManagerInstance
+}
+
+export interface IPromptManagerInstance {
+  getAllPromptCollectionJSON(): IPromptCollection[]
+}
+
+export interface IPromptCollection {
+  promptSteps: IPromptStep[]
+  viewStatePath: string
+}
+
+export interface IPromptStep {
+  prompts: IPrompt[]
+}
+
+export interface IPrompt {
+  caption: string
+  promptType: string
+  uiControlType: string
+  operator: string
+  dataType: IPromptDataType
+  currentValues: IPromptCurrentValue
+
+  viewStatePath?: string
+}
+
+export interface IPromptDataType {
+  displayColumnFormula: string
+}
+
+export interface IPromptCurrentValue {
+  values: IPromptValue[]
+}
+
+export interface IPromptValue {
+  caption: string
+  codeValue: any
 }
